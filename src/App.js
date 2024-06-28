f4bff817b567783b1cbade74ed165bc605f0bb82
Golovolastik <yankovich.aliaksei@gmail.com>
2024-06-29 00:44:37 +0300

    button styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {useEffect} from "react";
-const tg = window.Telegram.WebApp;
 
 function App() {
 
@@ -8,14 +7,9 @@ function App() {
       tg.ready();
   }, []);
 
-  const onClose = () => {
-    tg.close();
-  }
-
   return (
     <div className="App">
       work
-        <button onClick={onClose}>Close</button>
     </div>
   );
 }
